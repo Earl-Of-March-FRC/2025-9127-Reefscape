ac1069b2de60f7add55df8dc6981cd937eadf42a
Bruce Zhang <bqzh2019@gmail.com>
2025-02-01 14:01:07 -0500

    Added auto routines to robot container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,7 +6,7 @@ package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
-import frc.robot.commands.Drive;
+import frc.robot.commands.DriveFieldOriented;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.ExampleSubsystem;
