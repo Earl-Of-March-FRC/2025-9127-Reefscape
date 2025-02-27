9449ff3c7f26467a210e30718fd7cbec04f69081
Sadra Hemmati <sadra.hemmati@gmail.com>
2025-02-27 03:08:02 -0500

    Refactored Elevator to use Spark Maxes + a through bore encoder, added limit switches, added a second PID controller for up/down movement

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import com.revrobotics.spark.ClosedLoopSlot;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -19,25 +21,48 @@ public final class Constants {
   }
   public static class ElevatorConstants{
 
-    public static final double GEAR_RATIO = 10.71; 
-    public static final int COUNTS_PER_REV = 4096;
-    public static final double INCHES_PER_REV = 6; //NEEDS TESTING SINCE ITS NOT A WHEEL
+    //TODO verify gear ratio
+    public static final double GEAR_RATIO = 10;
 
-    public static final double COUNTS_TO_INCHES_CONVERSION = INCHES_PER_REV * Math.PI / COUNTS_PER_REV / GEAR_RATIO;
+    //TODO confirm counts per rev, this may need tobe changed to pulses per rev 
+    public static final int COUNTS_PER_REV = 8192;
 
-    public static final int MOTOR_PORT = 0;
+    //TODO confirm that this is the correct sprocket diameter
+    public static final double SPROCKET_DIAMETER_INCHES = 3.98; //NEEDS TESTING SINCE ITS NOT A WHEEL
 
-    public static final int PID_SLOT = 0;
-    public static final double P = 0;
-    public static final double I = 0;
-    public static final double D = 0;
-    public static final double F = 0;
+    public static final double COUNTS_TO_INCHES_CONVERSION = SPROCKET_DIAMETER_INCHES * Math.PI / COUNTS_PER_REV / GEAR_RATIO;
+    public static final double COUNTS_TO_METERS_CONVERSION = COUNTS_TO_INCHES_CONVERSION/39.37;
+
+    public static final int LEADER_PORT = 0;
+    public static final int FOLLOWER_PORT = 1;
+
+    public static final double P_DOWN = 0;
+    public static final double I_DOWN = 0;
+    public static final double D_DOWN = 0;
+    public static final double F_DOWN = 0;
+    
+    public static final double P_UP = 0;
+    public static final double I_UP = 0;
+    public static final double D_UP = 0;
+    public static final double F_UP = 0;
+
+    public static final ClosedLoopSlot PID_SLOT_DOWN = ClosedLoopSlot.kSlot0;
+    public static final ClosedLoopSlot PID_SLOT_UP = ClosedLoopSlot.kSlot1;
 
     public static final double INTAKE_POSITION = 0;
     public static final double L1_POSITION= 0;
     public static final double L2_POSITION= 0;
     public static final double L3_POSITION = 0;
     public static final double L4_POSITION = 0;
+
+    public static final int ENCODER_PORT_1 = 0;
+    public static final int ENCODER_PORT_2 = 1;
+
+    public static final int LOW_LIMIT_CHANNEL = 2;
+    public static final int HIGH_LIMIT_CHANNEL = 3;
+
+    //TODO mesure max position when elevator is fully extended
+    public static final double MAX_POSITION = 0;
 
   }
 }
--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -4,63 +4,110 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
+import frc.robot.Constants.ElevatorConstants;
 
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.FeedbackDevice;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
 
 
 public class Elevator extends SubsystemBase {
-  private final WPI_TalonSRX elevatorMotor;
+
+  private final SparkMax elevatorLeader;
+  private final SparkMax elevatorFollower;
+
+  private final RelativeEncoder encoder;
+
+  private final DigitalInput lowLimitSwitch;
+  private final DigitalInput highLimitSwitch;
 
   /** Creates a new Elevator. */
   
   public Elevator() {
+    elevatorLeader = new SparkMax(ElevatorConstants.LEADER_PORT, MotorType.kBrushless);
+    elevatorFollower = new SparkMax(ElevatorConstants.FOLLOWER_PORT, MotorType.kBrushless);
 
-    elevatorMotor = new WPI_TalonSRX(Constants.ElevatorConstants.MOTOR_PORT);
+    lowLimitSwitch = new DigitalInput(ElevatorConstants.LOW_LIMIT_CHANNEL);
+    highLimitSwitch = new DigitalInput(ElevatorConstants.HIGH_LIMIT_CHANNEL);
 
-    // Configure the encoder
-    elevatorMotor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, Constants.ElevatorConstants.PID_SLOT, 10);
-    elevatorMotor.configSelectedFeedbackCoefficient(Constants.ElevatorConstants.COUNTS_TO_INCHES_CONVERSION, Constants.ElevatorConstants.PID_SLOT, 10);
-    // Configure PID coefficients
-    elevatorMotor.config_kP(Constants.ElevatorConstants.PID_SLOT, Constants.ElevatorConstants.P, 10);
-    elevatorMotor.config_kI(Constants.ElevatorConstants.PID_SLOT, Constants.ElevatorConstants.I, 10);
-    elevatorMotor.config_kD(Constants.ElevatorConstants.PID_SLOT, Constants.ElevatorConstants.D, 10);
-    elevatorMotor.config_kF(Constants.ElevatorConstants.PID_SLOT, Constants.ElevatorConstants.F, 10);
+    encoder = elevatorLeader.getAlternateEncoder();
+    encoder.setPosition(0);
 
-    // Set neutral mode
-    elevatorMotor.setNeutralMode(NeutralMode.Brake);
+    SparkMaxConfig leaderConfig = new SparkMaxConfig();
+    SparkMaxConfig followerConfig = new SparkMaxConfig();
+
+    leaderConfig
+        .smartCurrentLimit(40)
+        .inverted(false)
+        .idleMode(IdleMode.kBrake);
+    leaderConfig.encoder
+        .positionConversionFactor(ElevatorConstants.COUNTS_TO_METERS_CONVERSION);
+    leaderConfig.closedLoop
+        .feedbackSensor(FeedbackSensor.kAlternateOrExternalEncoder)
+        .pidf(ElevatorConstants.P_UP, ElevatorConstants.I_UP, ElevatorConstants.D_UP, ElevatorConstants.F_UP, ElevatorConstants.PID_SLOT_UP)
+        .pidf(ElevatorConstants.P_DOWN, ElevatorConstants.I_DOWN, ElevatorConstants.D_DOWN, ElevatorConstants.F_DOWN, ElevatorConstants.PID_SLOT_DOWN)
+        .outputRange(-1, 1, ElevatorConstants.PID_SLOT_UP)
+        .outputRange(-1, 1, ElevatorConstants.PID_SLOT_DOWN);
+
+    followerConfig
+        .smartCurrentLimit(40)
+        .inverted(false)
+        .idleMode(IdleMode.kBrake);
+    followerConfig.follow(ElevatorConstants.LEADER_PORT, true);
+
+    elevatorLeader.configure(leaderConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    elevatorFollower.configure(followerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    if (lowLimitSwitch.get() || highLimitSwitch.get()) {
+      setSpeed(0);
+    }
+
+    if (lowLimitSwitch.get()) {
+      encoder.setPosition(0);
+    }
+
+    if (highLimitSwitch.get()) {
+      encoder.setPosition(ElevatorConstants.MAX_POSITION);
+    }
   }
 
-
   public void setSpeed(double speed) {
-    elevatorMotor.set(speed);
+    elevatorLeader.set(speed);
   }
 
   public void setPosition(double position){
-    elevatorMotor.set(ControlMode.Position, position);
+    SparkClosedLoopController controller = elevatorLeader.getClosedLoopController();
+
+    //Use the appropriate controller based on direction (up or down)
+    if (getPosition() <= position) {
+      controller.setReference(position, ControlType.kPosition, ElevatorConstants.PID_SLOT_UP);
+    }
+    else {
+      controller.setReference(position, ControlType.kPosition, ElevatorConstants.PID_SLOT_DOWN);
+    }
   }
 
   public double getPosition(){
-    return elevatorMotor.getSelectedSensorPosition();
-  }
-
-  public double getEncoderPosition(){
-    return elevatorMotor.getSelectedSensorPosition();
+    return encoder.getPosition();
   }
 
   public void setEncoderPosition(double position){
-    elevatorMotor.setSelectedSensorPosition(position);
+    encoder.setPosition(position);
   }
 
 }
